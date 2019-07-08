978c357294aff0227324629a589da0159ab07656
Javier Lillo <javierlillo.e@gmail.com>
2019-07-08 11:51:09 -0400

    MAXST listo

--- Prueba3/Prueba4.c
+++ Prueba3/Prueba4.c
@@ -4,6 +4,67 @@
 
 char labels[5] = {'A', 'B','C','D','E'};
 
+struct tree_node *crear_nodo_tree(char data) //arbol
+{
+    struct tree_node *temp = (struct tree_node*)malloc(sizeof(tree_node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+struct tree_node *buscar_nodo_tree(struct tree_node *Tree,char label)
+{
+    if( Tree !=NULL)
+    {
+        if(Tree->data == label)
+        {
+            return Tree;
+        }
+        else
+        {
+            struct tree_node *Left = buscar_nodo_tree(Tree->left,label);
+            struct tree_node *Right = buscar_nodo_tree(Tree->right,label);
+            if(Left!= NULL)
+            {
+                return Left;
+            }
+            else if(Right!=NULL)
+            {
+                return Right;
+            }
+        }
+    }
+    return NULL;
+}
+
+struct list_node *crear_nodo_list(char data) //lista
+{
+    struct list_node *temp = (struct list_node*)malloc(sizeof(list_node));
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+
+struct list_node *agrega_nodo_list(struct list_node* L,char data) //lista
+{
+    if(L != NULL)
+    {
+        struct list_node* temp = L;
+        while (temp->next != NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next = nuevo_nodo(data);
+    }
+    else
+    {
+        L = nuevo_nodo(data);
+    }
+    return L;
+    
+}
+
 struct tree_node* MaxST(int graph[5][5], int size)
 {   
     /*
@@ -16,36 +77,52 @@ struct tree_node* MaxST(int graph[5][5], int size)
    {
        return NULL;
    }
-
-   struct L{
-       int valor;
-       char actual;
-       char unido;
-    };
-
-    for(int i=0;i<size;i++)
-    {
-        for(int j=0;j<size;j++)
-        {
-            if(graph[i][j]!=-1 || i == j)
-            {
-                for(int k=0;k<size*size;k++)
-                {
-                    struct L *list=(struct L*)malloc(sizeof(struct L));
-                    list[k].actual=labels[i];
-                    list[k].unido=labels[j];
-                    list[k].valor=graph[i][j];
-                }
-            }
+   else if(size<0)
+   {
+       struct tree_node *root=crear_nodo_tree(labels[0]);
+       int A[5];
+       A[0]=0;//la distancia de A a A es 0
+       int large=1; //Ya tengo un nodo
+       for(int i=0 ;i<size;i++)
+       {
+           graph[i][0]=-1;
+       }
+       while(large<size)
+       {
+           int m=-1,f=-1,t=-1;
+           for(int i=0;i<large;i++)
+           {
+               for(int j=0;j<size;j++)
+               {
+                   if(m<graph[A[i]][j])
+                   {
+                       m=graph[A[i]][j];
+                       f=A[i];
+                       t=j;
+                   }
+               }
+           }
+           A[large]=t;
+           if (t!=-1)
+           {
+               for (int i =0;i<size;i++)
+               {
+                   graph[i][t]=-1;
+               }
+           }
+           struct tree_node* temp =buscar_nodo_tree(root,labels[f]);
+           if(temp->left == NULL)
+           {
+               temp->left = crear_nodo_tree(labels[t]);
+           }
+           else
+           {
+               temp->right = crear_nodo_tree(labels[t]);
+           }
+           large++;           
         }
-    }
-
-    struct tree_node *root =NULL;
-
-    struct tree_node *temp=(struct tree_node*)malloc(sizeof(tree_node));
-    temp->data=labels[0];
-    temp->left=NULL;
-    temp->right=NULL;
+    return root;
+   }
 }
 
 struct list_node* Amigos_Sugeridos(int graph[5][5],int size, struct tree_node *tree, char name)
