8f4e51e7877f0fe7a5c7d0a5a0d95c2c8ccaf0b6
Javier Lillo <javierlillo.e@gmail.com>
2019-07-08 16:52:07 -0400

    Prueba Lista

--- Prueba3/Prueba4.c
+++ Prueba3/Prueba4.c
@@ -4,10 +4,10 @@
 
 char labels[5] = {'A', 'B','C','D','E'};
 
-struct tree_node *crear_nodo_tree(char data) //arbol
+struct tree_node *crear_nodo_tree(char label) //arbol
 {
     struct tree_node *temp = (struct tree_node*)malloc(sizeof(tree_node));
-    temp->data = data;
+    temp->data = label;
     temp->left = NULL;
     temp->right = NULL;
     return temp;
@@ -136,6 +136,148 @@ struct list_node* Amigos_Sugeridos(int graph[5][5],int size, struct tree_node *t
     Puntero p que apunta al inicio de la lista con los amigos sugeridos
     */
 
+   /*
+   Tengo que revisar el nodo del arbol que corresponda a la variable "name" y ver cuales son sus hijos y sus padres,
+   luego ver los nodos padre e hijos de el y ver si son amigos de "name" para agregarlos en la lista.
+    */
+   int case_null=1;
+    for(int i=0;i<size;i++)
+    {
+        if(name != labels[i])
+        {
+            case_null++;
+        }
+    }
 
-    return NULL;
+    //identificar el indice de "name" en "labels"
+    int indice_name;
+    int indice_friend;
+    for(int i=0;i<size;i++) 
+    {
+        if(name==labels[i])
+        {
+            indice_name=i;
+        }
+    }
+
+    //caso de que el "name" no esté en labels
+    if(case_null == size)
+    {
+        return NULL;
+    }
+    else //Caso en que el "name" está en labels
+    {
+        
+        struct list_node* lista_amigos=(struct list_node*)malloc(sizeof(list_node)); //Creo la lista de amigos sugeridos
+        struct tree_node* temp=buscar_nodo_tree(tree,name);
+        struct tree_node* temp1=(struct tree_node*)malloc(sizeof(tree_node));
+        if(temp!=tree)
+        {
+            if(temp ==tree->left)
+            {
+                temp1=tree;
+            }
+            else if(temp==tree->right)
+            {
+                temp1=tree;
+            }
+        }
+        if (temp->left !=NULL)
+        {
+            if(temp!=tree)
+            {
+                if(temp ==tree->left)
+                {
+                    temp1=tree;
+                }
+            }
+            else
+            {
+                temp1=temp->left;            
+            }
+            
+            temp1=temp->left;
+            if(temp1->left !=NULL)
+            {
+                struct tree_node* Posible_amigo=(struct tree_node*)malloc(sizeof(tree_node));
+                Posible_amigo=temp1->left;
+                for(int i=0;i<size;i++)
+                {
+                    if(Posible_amigo->data == labels[i])
+                    {
+                        indice_friend=i;
+                    }
+                }
+                if(graph[indice_name][indice_friend]!=-1)
+                {
+                    lista_amigos->data=labels[indice_friend];
+                }
+            }
+            struct tree_node* posible_amigo2=(struct tree_node*)malloc(sizeof(tree_node));
+            lista_amigos->next = posible_amigo2;
+            if (temp1->right !=NULL)
+            {
+               posible_amigo2=temp1->right;
+                for(int i=0;i<size;i++)
+                {
+                    if(posible_amigo2->data == labels[i])
+                    {
+                        indice_friend=i;
+                    }
+                }
+                if(graph[indice_name][indice_friend]!=-1)
+                {
+                    lista_amigos->data=labels[indice_friend];
+                }
+
+            }
+        }
+        else if(temp->right !=NULL)
+        {
+           if(temp!=tree)
+            {
+                if(temp ==tree->right)
+                {
+                    temp1=tree;
+                }
+            }
+            else
+            {
+                temp1=temp->right;            
+            }
+            if(temp1->left !=NULL)
+            {
+                struct tree_node* Posible_amigo=temp1->left;
+                for(int i=0;i<size;i++)
+                {
+                    if(Posible_amigo->data == labels[i])
+                    {
+                        indice_friend=i;
+                    }
+                }
+                if(graph[indice_name][indice_friend]!=-1)
+                {
+                    lista_amigos->data=labels[indice_friend];
+                }
+            }
+            struct tree_node* posible_amigo2=(struct tree_node*)malloc(sizeof(tree_node));
+            lista_amigos->next = posible_amigo2;
+            if (temp1->right !=NULL)
+            {
+                posible_amigo2=temp1->right;
+                for(int i=0;i<size;i++)
+                {
+                    if(posible_amigo2->data == labels[i])
+                    {
+                        indice_friend=i;
+                    }
+                }
+                if(graph[indice_name][indice_friend]!=-1)
+                {
+                    lista_amigos->data=labels[indice_friend];
+                }
+            }
+        }
+        return lista_amigos;
+    }
 }
